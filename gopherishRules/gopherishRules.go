ba2ddb17eb0a0d41f80b5e8b6a18564a009a1b52
dimitar.draganov <dimitar.g.draganov@gmail.com>
2021-09-20 22:38:08 +0300

    Added gopherish translation rules

---
+++ gopherishRules/gopherishRules.go
@@ -0,0 +1,20 @@
+package gopherishRules
+
+import "regexp"
+
+var Dictionary map[*regexp.Regexp]string
+
+func init() {
+	Dictionary = make(map[*regexp.Regexp]string, 3)
+
+	var re *regexp.Regexp
+
+	re = regexp.MustCompile(`^((?:[rRtTpPsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM]{1}qu)|(?:[qQrRtTpPsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM]+))(\w+)([^a-zA-Z]*)$`)
+	Dictionary[re] = "${2}${1}ogo${4}"
+
+	re = regexp.MustCompile(`^([aAeEiIoOuUyYwW]+\w+[^a-zA-Z]*)$`)
+	Dictionary[re] = "g${1}"
+
+	re = regexp.MustCompile(`^(xr\w+[^a-zA-Z]*)$`)
+	Dictionary[re] = "ge${1}"
+}
